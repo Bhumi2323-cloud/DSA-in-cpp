6c1c8e0c24aafef82e93c3bb00760e7ee92c865b
bhumi <bhumidhoundiyal23@gmail.com>
2026-09-04 18:39:49 +0530

    practised even/odd, positive negative, counting digits

---
+++ day3.cpp
@@ -0,0 +1,51 @@
+// to find whether a number is odd or even//
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a;
+//     cin>>a;
+//     if(a%2==0){
+//         cout<<"the number is even";
+//     }
+//     else{
+//         cout<<"the number is odd";
+//     }
+// }
+
+// to find wether the number is positive or negative//
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a;
+//     cin>>a;
+//     if(a>0){
+//         cout<<"the number is positive";
+//     }
+//     else if(a<0){ 
+//         cout<<"the number is negative";
+//     }
+//     else if(a==0){
+//         cout<<"the number is zero";
+//     }
+//     }
+
+//to find the number of digits in a given number//
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int a;
+//     cout << "enter a number: ";
+//     cin >> a;
+
+//     int count = 0;
+
+//     while (a != 0) {
+//         count = count + 1;
+//         a = a / 10;
+//     }
+
+//     cout << count;
+
+//     return 0;
+// }
